39dcaf83aec29d844c3fbae9ea0a604a5d85ee06
nathan-websculpt <websculpt.nathan@gmail.com>
2023-11-19 11:54:44 -0600

    nested table not looking great

---
+++ app/person-data/components/rows.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useState } from "react";
+import { userObj } from "../../../utils/person-data/data";
+
+interface RowsProps {
+  row: userObj;
+}
+export default function Rows(person: RowsProps) {
+  const [rowOpen, setRowOpen] = useState(false);
+  return (
+    <>
+      <tr className="text-sm" key={person.row.id.toString()}>
+        <td className="w-1/12 md:py-4">
+          <svg
+            className={`text-black w-6 h-6 z-40  ${
+              rowOpen ? "rotate-180" : "rotate-0"
+            }`}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            onClick={() => setRowOpen(!rowOpen)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </td>
+
+        <td className="w-1/12 md:py-4">{person.row.id.toString()}</td>
+        <td className="w-1/12 md:py-4">{person.row.name}</td>
+        <td className="w-1/12 md:py-4">{person.row.email}</td>
+        <td className="w-1/12 md:py-4">{person.row.phone}</td>
+
+
+
+        {/* drillable nested table */}
+        <tr>
+          <td colSpan={5} className="pl-10">
+            <h1 className={`text-xl ${rowOpen ? "block" : "hidden"}`}>
+              Addresses
+            </h1>
+          </td>
+        </tr>
+        <tr
+          className={`w-full overflow-hidden transition-[max-height] delay-1000 duration-1000 ease-in-out  ${
+            rowOpen ? "max-h-20" : "max-h-0"
+          }`}
+        >
+          <td colSpan={9}>
+            <table
+              className={`px-10 w-fit  ${rowOpen ? "block" : "hidden"} mx-auto`}
+            >
+              <thead>
+                <tr className="text-sm rounded-xl text-base-content">
+                  <th className="bg-primary">
+                    ID
+                  </th>
+                  <th className="bg-primary">
+                    Address
+                  </th>
+                  <th className="bg-primary">
+                    Zip
+                  </th>
+                  <th className="bg-primary">
+                    State
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {person.row.addresses?.map((addrData) => (
+                  <tr key={addrData.id}>
+                    <td className="px-4 py-3">{addrData?.id}</td>
+                    <td className="px-4 py-3">{addrData?.address}</td>
+                    <td className="px-4 py-3">{addrData?.zip}</td>
+                    <td className="px-4 py-3">{addrData?.state}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tr>
+    </>
+  );
+}
+// TODO: drillable table needs to display a user's list of addresses
+//https://www.tailwindtap.com/components/table/nested-table
